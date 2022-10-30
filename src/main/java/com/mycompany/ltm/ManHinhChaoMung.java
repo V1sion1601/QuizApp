3f9edafa8a4ea2e23decd2d8b2d2ee34d697f337
V1sion <64670890+V1sion1601@users.noreply.github.com>
2022-10-30 13:54:13 +0700

    Fix Package Name

--- src/main/java/com/mycompany/ltm/ManHinhChaoMung.java
+++ src/main/java/com/mycompany/ltm/ManHinhChaoMung.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package ltm_detai11;
+package com.mycompany.ltm;
 
 import java.awt.Color;
 
