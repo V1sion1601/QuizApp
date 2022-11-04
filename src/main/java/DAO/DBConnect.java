02eebf54d2f5c3723e1c89a7ab1db7a437fb7dda
Beminhlun678 <113537921+Beminhlun678@users.noreply.github.com>
2022-11-05 01:04:06 +0700

    Next

--- src/main/java/DAO/DBConnect.java
+++ src/main/java/DAO/DBConnect.java
@@ -12,107 +12,125 @@ import javax.swing.JOptionPane;
  * @author admin
  */
 public class DBConnect {
-
-    private Connection conn = null;
-    private Statement stmt = null;
-    private ResultSet rs = null;
-
-    private String DBName = null;
-    private String username = null;
-    private String password = null;
-
-    private final String ipAddress = "localhost:3306";
-
-    public static String dbUrl = "jdbc:mysql://hostname:3306/laptrinhmang";
-
-    public DBConnect() {
-        if (checkDriver() == true) {
-            this.DBName = "LTM";
-            this.username = "root";
-            this.password = null;
-            getConnection();
-        }
-    }
-
-    public DBConnect(String DBName) {
-        if (checkDriver() == true) {
-            this.DBName = "databaseName";
-            getConnection();
-        }
-    }
-
-    private boolean checkDriver() {
+    
+    public class Connection {
+    private final static String JDBC = "com.mysql.cj.jdbc.Driver";
+    private final static String DATA = "jdbc:mysql://localhost:3306/gamemomo";
+    
+    public static java.sql.Connection connection() {
+        java.sql.Connection con = null;
+        java.beans.Statement sta = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            return true;
-        } catch (ClassNotFoundException e) {
-            JOptionPane.showMessageDialog(null, "Lỗi!!! Không tìm thấy MySql Driver.");
-            System.err.print(e);
-            return false;
+            Class.forName(JDBC);
+            con = DriverManager.getConnection(DATA, "root", "");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Không tìm thấy driver. Chi tiết:  " + ex.getMessage());
+        } catch (SQLException ex) {
+            System.err.println(ex);
         }
+        return con;
     }
 
-    public void getConnection() {
-        try {
-            String username = "root";
-            String password = null;
-            conn = DriverManager.getConnection(dbUrl);
-            Connection conn = DriverManager.getConnection(dbUrl);
-            stmt = conn.createStatement();
-            System.out.println("connect successfully!");
-        } catch (SQLException e) {
-            System.out.println("Lỗi!!! Không thể kết nối tới " + DBName);
-            System.err.println(e);
-        }
-    }
-
-    private boolean checkConnection() {
-        if (conn == null || stmt == null) {
-            System.out.println("Lỗi!!! Chưa thiết lập kết nối tới " + DBName + ". Vui lòng đăng nhập để thiết lập kết nối!");
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public ResultSet SQLQuery(String query) {
-        if (checkConnection()) {
-            try {
-                rs = stmt.executeQuery(query);
-                System.out.println(query + " truy vấn thành công!");
-                return rs;
-            } catch (SQLException e) {
-                System.out.println("Lỗi!!! Không thể truy vấn dữ liệu");
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    public boolean SQLUpdate(String update) {
-        try {
-            stmt.executeUpdate(update);
-            System.out.println(update + " cập nhật thành công!");
-            return true;
-        } catch (SQLException e) {
-            System.err.println(e);
-            System.out.println("Lỗi!!! Không thể cập nhật dữ liệu");
-            return false;
-        }
-    }
-
-    public void closeConnection() {
-        try {
-            if (conn != null) {
-                conn.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            System.out.println("Đã ngắt kết nối tới Database.");
-        } catch (SQLException e) {
-            System.out.println("Lỗi!!! Không thể đóng kết nối tới " + DBName);
-        }
-    }
+//    private Connection conn = null;
+//    private Statement stmt = null;
+//    private ResultSet rs = null;
+//
+//    private String DBName = null;
+//    private String username = null;
+//    private String password = null;
+//
+//    private final String ipAddress = "localhost:3306";
+//
+//    public static String dbUrl = "jdbc:mysql://hostname:3306/laptrinhmang";
+//
+//    public DBConnect() {
+//        if (checkDriver() == true) {
+//            this.DBName = "LTM";
+//            this.username = "root";
+//            this.password = null;
+//            getConnection();
+//        }
+//    }
+//
+//    public DBConnect(String DBName) {
+//        if (checkDriver() == true) {
+//            this.DBName = "databaseName";
+//            getConnection();
+//        }
+//    }
+//
+//    private boolean checkDriver() {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            return true;
+//        } catch (ClassNotFoundException e) {
+//            JOptionPane.showMessageDialog(null, "Lỗi!!! Không tìm thấy MySql Driver.");
+//            System.err.print(e);
+//            return false;
+//        }
+//    }
+//
+//    public void getConnection() {
+//        try {
+//            String username = "root";
+//            String password = null;
+//            conn = DriverManager.getConnection(dbUrl);
+//            Connection conn = DriverManager.getConnection(dbUrl);
+//            stmt = conn.createStatement();
+//            System.out.println("connect successfully!");
+//        } catch (SQLException e) {
+//            System.out.println("Lỗi!!! Không thể kết nối tới " + DBName);
+//            System.err.println(e);
+//        }
+//    }
+//
+//    private boolean checkConnection() {
+//        if (conn == null || stmt == null) {
+//            System.out.println("Lỗi!!! Chưa thiết lập kết nối tới " + DBName + ". Vui lòng đăng nhập để thiết lập kết nối!");
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//
+//    public ResultSet SQLQuery(String query) {
+//        if (checkConnection()) {
+//            try {
+//                rs = stmt.executeQuery(query);
+//                System.out.println(query + " truy vấn thành công!");
+//                return rs;
+//            } catch (SQLException e) {
+//                System.out.println("Lỗi!!! Không thể truy vấn dữ liệu");
+//                return null;
+//            }
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public boolean SQLUpdate(String update) {
+//        try {
+//            stmt.executeUpdate(update);
+//            System.out.println(update + " cập nhật thành công!");
+//            return true;
+//        } catch (SQLException e) {
+//            System.err.println(e);
+//            System.out.println("Lỗi!!! Không thể cập nhật dữ liệu");
+//            return false;
+//        }
+//    }
+//
+//    public void closeConnection() {
+//        try {
+//            if (conn != null) {
+//                conn.close();
+//            }
+//            if (stmt != null) {
+//                stmt.close();
+//            }
+//            System.out.println("Đã ngắt kết nối tới Database.");
+//        } catch (SQLException e) {
+//            System.out.println("Lỗi!!! Không thể đóng kết nối tới " + DBName);
+//        }
+//    }
 }
--- src/main/java/DTO/QuestionSetDTO.java
+++ src/main/java/DTO/QuestionSetDTO.java
@@ -9,6 +9,42 @@ package DTO;
  *
  * @author ADMIN
  */
-public class QuestionSetdto {
+public class QuestionSetDTO {
+    Integer ID_QuestionSet, Point, Time;
+
+    public QuestionSetDTO() {
+    }
+
+    public QuestionSetDTO(Integer ID_QuestionSet, Integer Point, Integer Time) {
+        this.ID_QuestionSet = ID_QuestionSet;
+        this.Point = Point;
+        this.Time = Time;
+    }
+
+    public Integer getID_QuestionSet() {
+        return ID_QuestionSet;
+    }
+
+    public void setID_QuestionSet(Integer ID_QuestionSet) {
+        this.ID_QuestionSet = ID_QuestionSet;
+    }
+
+    public Integer getPoint() {
+        return Point;
+    }
+
+    public void setPoint(Integer Point) {
+        this.Point = Point;
+    }
+
+    public Integer getTime() {
+        return Time;
+    }
+
+    public void setTime(Integer Time) {
+        this.Time = Time;
+    }
+
     
+
 }
